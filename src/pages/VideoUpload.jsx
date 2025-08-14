4c315797b59f82df107e173888928cbdc707a0a6
stevehunt70 <steve@infineo-solutions.co.uk>
2025-08-14 22:31:58 +0100

    correct user login

--- src/pages/VideoUpload.jsx
+++ src/pages/VideoUpload.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+/*import axios from "axios";*/
 import "./VideoUpload.css"; 
 
 const VideoUpload = () => {
@@ -15,7 +15,7 @@ const VideoUpload = () => {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = async (e) => {
+  /*const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
@@ -30,6 +30,7 @@ const VideoUpload = () => {
       alert("Error uploading video");
     }
   };
+  */
 
   return (
     <div className="upload-container">
