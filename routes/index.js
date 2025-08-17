0555d32c7a6d6e9e59f05c2658622b81c0579ca2
stevehunt70 <steve@infineo-solutions.co.uk>
2025-08-17 22:34:36 +0100

    hamburger menu now receiving data

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get("/api", (req, res) => {
 
 router.use("/api/categories", categoryRoutes);
 router.use("/api/posts", postRoutes);
-router.use("/api/users", userRoutes);
+router.use("/user", userRoutes);
 router.use('/api/videos', videoRoutes);
 router.use('/api/video_comments', videoCommentRoutes);
 router.use('/api/comment_threads', commentThreadRoutes);
@@ -29,33 +29,3 @@ router.use('/api/playlist_videos', playlistVideoRoutes);
 router.use('/api/view_history', viewHistoryRoutes);
 
 module.exports = router;
-
-
-// const router = require('express').Router();
-// const userRoutes = require('./user');
-// const videoRoutes = require('./video-routes');
-// const videoCommentRoutes = require('./video_comment-routes');
-// const commentThreadRoutes = require('./comment_thread-routes');
-// const playlistRoutes = require('./playlist-routes');
-// const playlistVideoRoutes = require('./playlist_video-routes');
-// const viewHistoryRoutes = require('./view_history-routes');
-
-// router.get("/api", (req, res) => {
-//   res.json({ message: "Welcome to the API" });
-// });
-
-// // Use these route files with their respective endpoints
-// router.use('/users', userRoutes);
-// router.use('/videos', videoRoutes);
-// router.use('/video_comments', videoCommentRoutes);
-// router.use('/comment_threads', commentThreadRoutes);
-// router.use('/playlists', playlistRoutes);
-// router.use('/playlist_videos', playlistVideoRoutes);
-// router.use('/view_history', viewHistoryRoutes);
-
-// // For any request that doesn't match a defined route, send a 404 error.
-// // router.use((req, res) => {
-// //   res.status(404).end();
-// // });
-
-// module.exports = router;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3002',
+  resolve: {
+    alias: {
+      react: fileURLToPath(new URL("./node_modules/react", import.meta.url)),
+      "react-dom": fileURLToPath(new URL("./node_modules/react-dom", import.meta.url)),
+      "react/jsx-runtime": fileURLToPath(new URL("./node_modules/react/jsx-runtime", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3002",
+    },
+  },
+  
 });
